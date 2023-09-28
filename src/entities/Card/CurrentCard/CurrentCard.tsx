c4e1cd50070cadfbe6ff4f780045815233e4316e
AlexandrKataev <dkeyman@mail.ru>
2023-09-29 01:14:54 +0300

    code splitting

--- src/entities/Card/CurrentCard/CurrentCard.tsx
+++ src/entities/Card/CurrentCard/CurrentCard.tsx
@@ -1,6 +1,6 @@
-import { MasterCardIcon, Button } from '@ui';
 import styles from './CurrentCard.module.scss';
 import { CardMenu } from './CardMenu';
+import { CardImage } from './CardImage';
 
 export const CurrentCard = () => {
   return (
@@ -9,16 +9,7 @@ export const CurrentCard = () => {
         <div className={styles.title}>Cards</div>
         <div className={styles.showmore}>Show All</div>
       </div>
-      <div className={styles.card}>
-        <div className={styles.name}>
-          <div>Alexander Kataev</div>
-          <MasterCardIcon className={styles.master} />
-        </div>
-        <div className={styles.cardinfo}>
-          <div className={styles.number}>0123 4567 8910</div>
-          <Button title="CCV" variant="gray" height={32} width={65} fontSize={14} />
-        </div>
-      </div>
+      <CardImage />
       <CardMenu />
     </div>
   );
